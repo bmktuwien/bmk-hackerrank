eeba378fe5dbd1b2a274ae26088b094ada83c4e4
Kim Bong Min <bong-min.kim@rise-world.com>
2018-09-10 10:57:36 +0200

    Bug fixes

--- max_palindromes/Solution.hs
+++ max_palindromes/Solution.hs
@@ -5,7 +5,9 @@ module Main where
 import           Control.Monad
 import qualified Data.ByteString.Char8 as C
 import           Data.Bits
+import           Data.List
 import qualified Data.Map.Strict       as Map
+import           Debug.Trace
 
 
 factorials :: Int -> Int -> [Int]
@@ -29,50 +31,64 @@ modExp b e m = go b e 1
 initFreqMap :: C.ByteString -> Map.Map Char [Int]
 initFreqMap inp = go map1 map2 inp
   where
-    map1 = Map.fromList $ zip ['a'..'z'] [0..]
+    map1 = Map.fromList $ zip ['a'..'z'] $ repeat 0
     map2 = Map.fromList $ zip ['a'..'z'] $ repeat []
 
-    go m1 m2 inp'
-      | C.null inp' = m2
-      | otherwise    = go m1' m2' $ C.tail inp'
+    go m1 m2 inp
+      | C.null inp = m2
+      | otherwise  = go m1' m2' $ C.tail inp
       where
-        w   = C.head inp
-        m1' = Map.update (\v -> Just (v+1)) w m1
-        m2' = Map.update (\v -> liftM (:v) $ Map.lookup w m1') w m2
+        m1' = Map.update (\v -> Just $ v+1) (C.head inp) m1
+        m2' = foldl' (\m w -> Map.update (\v -> liftM ((v ++) . (:[])) $ Map.lookup w m1') w m) m2 ['a'..'z']
+
 
 query :: Int -> Int -> Int -> Map.Map Char [Int] -> [Int] -> Int
-query l r m freqMap facts = undefined
+query l r m freqMap facts
+  | x > 1     = (x * y) `mod` m
+  | otherwise = y
   where
+    calcCnt cs = cr - cl
+      where
+         cl | l > 1     = cs !! (l-2)
+            | otherwise = 0
+
+         cr = cs !! (r-1)
+
     f1 acc cs
-      | even cnt  = 0
+      | even cnt = acc
       | otherwise = acc + 1
       where
-        cl | l > 1     = cs !! (l-2)
-           | otherwise = 0
+        cnt = calcCnt cs
 
-        cr = cs !! (r-2)
+    f2 (acc1,acc2) cs
+      | cnt < 2   = (acc1 ,acc2)
+      | otherwise = (acc1',acc2')
+      where
+        cnt = calcCnt cs
 
-        cnt = cr - cl
+        n = cnt `div` 2
 
-    --f2 acc 0 = acc
-    --f2 acc 1 = acc
-    --f2 (acc1,acc2) v = (acc1+v',acc2)
-    --  where
-    --    v' = v `div` 2
+        acc1' = acc1 + n
+        r = choose acc1' n
+        acc2' = (acc2 * r) `mod` m
 
-    choose n k = (f1 * t) `mod` m
+
+    choose n k
+      | n < k = 0
+      | otherwise = (f1 * t) `mod` m
       where
         f1 = facts !! n
-        f2 = facts !! (n-k)
+        f2 = facts !! k
+        f3 = facts !! (n-k)
 
-        i1 = (modExp f1 (m-2) m) `mod` m
-        i2 = (modExp f2 (m-2) m) `mod` m
+        i1 = (modExp f2 (m-2) m) `mod` m
+        i2 = (modExp f3 (m-2) m) `mod` m
 
         t = (i1 * i2) `mod` m
 
 
     x = Map.foldl' f1 0 freqMap
-    --(_, y) = Map.foldl' f2 (0,1) freqMap
+    y = snd $ Map.foldl' f2 (0,1) freqMap
 
 
 main :: IO()
@@ -81,7 +97,7 @@ main = do
     q   <- readLn :: IO Int
 
     let modulo  = 1000000007
-    let facts   = factorials (C.length inp) modulo
+    let facts   = reverse $ factorials (C.length inp) modulo
     let freqMap = initFreqMap inp
 
     forM_ [1..q] $ \_ -> do
