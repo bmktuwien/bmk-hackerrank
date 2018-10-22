44280261d63ac409d195afe7cb0bd39809f39248
Kim Bong Min <bong-min.kim@rise-world.com>
2018-10-22 22:31:59 +0200

    intermediate commit

--- common_child/solution.cc
+++ common_child/solution.cc
@@ -1,27 +1,32 @@
+#include <algorithm>
 #include <vector>
 #include <iostream>
 #include <map>
+#include <set>
 
-std::vector<std::vector<long>> calc_freq(const std::string& inp) {
-    std::vector<std::vector<long>> freq_map;
+using FreqMap = std::vector<std::vector<long>>;
 
-    auto l = inp.length();
+FreqMap calc_freq(const std::string& inp) {
+    FreqMap freq_map(26);
 
-    for (int i = 0; i < 26; i++) {
-        freq_map.emplace_back(std::vector<long>(inp.size(), 0));
-    }
-
-    std::vector<long> acc_map(26, 0);
     for (size_t i = 0; i < inp.size(); i++) {
-        acc_map[inp[i]-'a']++;
-
-        for (int j = 0; j < 26; j++) {
-            freq_map[j][i] = acc_map[j];
-        }
+        freq_map[inp[i]-'A'].emplace_back(i);
     }
 
     return freq_map;
 }
+
+int next_pos(char c, int pos, const FreqMap& freq_map) {
+    auto &v = freq_map[c-'A'];
+    auto it = std::lower_bound(v.begin(), v.end(), pos+1);
+
+    if (it != v.end()) {
+        return *it;
+    } else {
+        return -1;
+    }
+}
+
 
 int main() {
     std::string s1;
@@ -31,11 +36,35 @@ int main() {
 
     auto freq_map = calc_freq(s1);
 
-    std::vector<std::pair<int,int>> cache(26, std::make_pair(0,0));
+    std::set<std::pair<int,int>> s;
+    s.insert(std::make_pair(0,-1));
+
 
     int res = 0;
-    for (int i = 0; i < 26; i++) {
 
+    for (int i = 0; i < s2.size(); i++) {
+        std::vector<std::pair<int,int>> v;
+
+        for (const auto& p : s) {
+            int q = next_pos(s2[i], p.second, freq_map);
+            if (q != -1) {
+                int r = p.first+1;
+                v.push_back(std::make_pair(r,q));
+
+                if (r > res) {
+                    res = r;
+                }
+            }
+        }
+
+        for (const auto& p : v) {
+            s.insert(p);
+        }
     }
 
+    /*for (int j = 0; j < dp_map1.size(); j++) {
+        std::cout << "[" << dp_map1[j] << "," << dp_map2[j] << "] ";
+    }
+    std::cout << std::endl;*/
+    std::cout << res << std::endl;
 }
