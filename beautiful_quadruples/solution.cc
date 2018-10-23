9526ac0bd0aea927ddb41ce06729b12c12aa10fc
Kim Bong Min <bong-min.kim@rise-world.com>
2018-10-23 20:10:11 +0200

    Implement brute force solution

--- beautiful_quadruples/solution.cc
+++ beautiful_quadruples/solution.cc
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iostream>
 #include <map>
+#include <set>
 
 
 int main() {
@@ -12,9 +13,19 @@ int main() {
     std::vector<int> v({a,b,c,d});
     std::sort(v.begin(), v.end());
 
+    int res = 0;
+
     for (int a = 1; a <= v[0]; a++) {
         for (int b = a; b <= v[1]; b++) {
-            std::cout << (a^b) << std::endl;
+            for (int c = b; c <= v[2]; c++) {
+                for (int d = c; d <= v[3]; d++) {
+                    if ((a^b^c^d) != 0) {
+                        res++;
+                    }
+                }
+            }
         }
     }
+
+    std::cout << res << std::endl;
 }
