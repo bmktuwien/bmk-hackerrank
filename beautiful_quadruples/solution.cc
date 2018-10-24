20104142d08cb3eafac8664fa612097f2f0f5f5a
Kim Bong Min <bong-min.kim@rise-world.com>
2018-10-24 13:18:37 +0200

    intermediate commit

--- beautiful_quadruples/solution.cc
+++ beautiful_quadruples/solution.cc
@@ -5,21 +5,22 @@
 #include <set>
 
 
-int main() {
-    int a, b, c, d;
-
-    std::cin >> a >> b >> c >> d;
-
+void brute_force_debug(int a, int b, int c, int d) {
     std::vector<int> v({a,b,c,d});
     std::sort(v.begin(), v.end());
 
     std::map<int,int> m;
+    int res = 0;
 
     for (int a = 1; a <= v[0]; a++) {
         for (int b = a; b <= v[1]; b++) {
             for (int c = b; c <= v[2]; c++) {
                 for (int d = c; d <= v[3]; d++) {
                     m[a^b^c^d]++;
+
+                    if ((a^b^c^d) != 0) {
+                        res++;
+                    }
                 }
             }
         }
@@ -28,4 +29,36 @@ int main() {
     for (auto p : m) {
         std::cout << p.first << ": " << p.second << std::endl;
     }
+
+    std::cout << res << std::endl;
+}
+
+int main() {
+    int a, b, c, d;
+
+    std::cin >> a >> b >> c >> d;
+
+    std::vector<int> v({a,b,c,d});
+    std::sort(v.begin(), v.end());
+
+    long total_cnt = 0;
+    int z = 0;
+
+    for (int a = 1; a <= v[0]; a++) {
+        for (int b = a; b <= v[1]; b++) {
+            for (int c = b; c <= v[2]; c++) {
+                int x = a^b^c;
+
+                if (x >= c && x <= v[3]) {
+                    z++;
+                }
+
+                total_cnt += (v[3]-c+1);
+            }
+        }
+    }
+
+    std::cout << (total_cnt - z) << std::endl;
+
+    //brute_force_debug(a,b,c,d);
 }
