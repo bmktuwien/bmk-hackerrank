4d82a7d8b57b1f82d4f6e07caa3c52ed2214494d
Kim Bong Min <bong-min.kim@rise-world.com>
2018-10-28 20:38:49 +0100

    broken again

--- build_a_string/solution.cc
+++ build_a_string/solution.cc
@@ -6,7 +6,7 @@
 #include <random>
 
 
-long build_brute2(int a, int b, const std::string& s, int i, std::vector<long>& cache) {
+long build_brute(int a, int b, const std::string& s, int i, std::vector<long>& cache) {
     long res = 0;
 
     if (i >= s.size()) {
@@ -17,45 +17,21 @@ long build_brute2(int a, int b, const std::string& s, int i, std::vector<long>& 
         return cache[i];
     }
 
-    res = std::min(a,b) + build_brute2(a, b, s, i+1, cache);
+    res = std::min(a,b) + build_brute(a, b, s, i+1, cache);
 
     auto s1 = s.substr(0,i);
 
     int q = 2;
     while (s1.find(s.c_str()+i, 0, q) != std::string::npos) {
-        long r = b + build_brute2(a, b, s, i+q, cache);
-        if (r < res) {
-            res = r;
-        }
-
         q++;
+    }
+
+    long r = b + build_brute(a, b, s, i+q-1, cache);
+    if (r < res) {
+        res = r;
     }
 
     cache[i] = res;
-
-
-    return res;
-}
-
-long build_brute(int a, int b, std::string s1, std::string s2) {
-    long res = 0;
-
-    if (s2.empty()) {
-        return 0;
-    }
-
-    res = a + build_brute(a, b, s1+s2[0], s2.substr(1));
-
-    int q = 1;
-    while (s1.find(s2.c_str(), 0, q) != std::string::npos) {
-        auto sub = s2.substr(0, q);
-        long r = b + build_brute(a, b, s1+sub, s2.substr(q));
-        if (r < res) {
-            res = r;
-        }
-
-        q++;
-    }
 
 
     return res;
@@ -73,8 +49,6 @@ int main() {
 
         std::vector<long> cache(input.size(),-1);
 
-
-        //std::cout << build_brute(a,b,"",input) << std::endl;
-        std::cout << build_brute2(a,b,input,0,cache) << std::endl;
+        std::cout << build_brute(a,b,input,0,cache) << std::endl;
     }
 }
