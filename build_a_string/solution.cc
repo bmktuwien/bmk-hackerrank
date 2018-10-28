d5983ffab07c247bbab9e0ffe2c7ac4b00388fa3
Kim Bong Min <bong-min.kim@rise-world.com>
2018-10-28 20:12:27 +0100

    intermediate commit

--- build_a_string/solution.cc
+++ build_a_string/solution.cc
@@ -5,54 +5,34 @@
 #include <set>
 #include <random>
 
-std::string random_string( size_t length )
-{
-    auto randchar = []() -> char
-                        {
-                            const char charset[] =
-                                "abcdefghijklmnopqrstuvwxyz";
-                            const size_t max_index = (sizeof(charset) - 1);
-                            return charset[ rand() % max_index ];
-                        };
-    std::string str(length,0);
-    std::generate_n( str.begin(), length, randchar );
-    return str;
-}
 
-long build(int a, int b, std::string& s) {
+long build_brute2(int a, int b, const std::string& s, int i, std::vector<long>& cache) {
     long res = 0;
 
-    int c = b/a+1;
-    res = a * std::min(c,(int)s.size());
-
-    std::string s2 = s.substr(0, c);
-    s.erase(0, c);
-
-    while (!s.empty()) {
-        int q = c;
-        bool found = false;
-
-        while (s2.find(s.c_str(), 0, q) != std::string::npos) {
-            found = true;
-            q++;
-        }
-
-        std::cout << "built=" << s2 << " rest=" << s << " fouund=" << found << std::endl;
-
-        if (!found) {
-            std::cout << "appending: " << s[0] << std::endl;
-            s2.push_back(s[0]);
-            s.erase(0, 1);
-
-            res += a;
-        } else {
-            std::cout << "copying: " << s.substr(0,q-1) << std::endl;
-            s2.append(s, 0, q-1);
-            s.erase(0, q-1);
-
-            res += b;
-        }
+    if (i >= s.size()) {
+        return 0;
     }
+
+    if (cache[i] != -1) {
+        return cache[i];
+    }
+
+    res = std::min(a,b) + build_brute2(a, b, s, i+1, cache);
+
+    auto s1 = s.substr(0,i);
+
+    int q = 2;
+    while (s1.find(s.c_str()+i, 0, q) != std::string::npos) {
+        long r = b + build_brute2(a, b, s, i+q, cache);
+        if (r < res) {
+            res = r;
+        }
+
+        q++;
+    }
+
+    cache[i] = res;
+
 
     return res;
 }
@@ -60,12 +40,10 @@ long build(int a, int b, std::string& s) {
 long build_brute(int a, int b, std::string s1, std::string s2) {
     long res = 0;
 
-
     if (s2.empty()) {
         return 0;
     }
 
-    std::cout << "s1=" << s1 << " s2=" << s2 << std::endl;
     res = a + build_brute(a, b, s1+s2[0], s2.substr(1));
 
     int q = 1;
@@ -83,21 +61,6 @@ long build_brute(int a, int b, std::string s1, std::string s2) {
     return res;
 }
 
-void find_bug() {
-    while (true) {
-        auto input = random_string(10);
-        int a = 7890;
-        int b = 7891;
-
-        auto r1 = build_brute(a,b,"",input);
-        auto r2 = build(a,b,input);
-
-        if (r1 != r2) {
-            std::cout << "found bug: " << input << std::endl;
-        }
-    }
-}
-
 int main() {
     int t;
     std::cin >> t;
@@ -108,7 +71,10 @@ int main() {
         std::string input;
         std::cin >> input;
 
-        std::cout << build_brute(a,b,"",input) << std::endl;
-        std::cout << build(a,b,input) << std::endl;
+        std::vector<long> cache(input.size(),-1);
+
+
+        //std::cout << build_brute(a,b,"",input) << std::endl;
+        std::cout << build_brute2(a,b,input,0,cache) << std::endl;
     }
 }
