9dbf56b1de7890f8068d6817bcf405d7d9b49782
Kim Bong Min <bong-min.kim@rise-world.com>
2019-03-26 22:05:25 +0100

    Little bit better

--- gena_hanoi/gena_hanoi.py
+++ gena_hanoi/gena_hanoi.py
@@ -9,11 +9,11 @@ target_sig = ''
 
 
 def get_sig(c):
-    sig = ''
+    sig = []
     for r in c:
-        sig += ''.join(map(str, r)).zfill(N)
+        sig += r + [0]*(N-len(r))
 
-    return sig
+    return tuple(sig)
 
 
 def solve(c):
@@ -26,24 +26,26 @@ def solve(c):
             for i in range(4):
                 for j in range(4):
                     if c[i] and (not c[j] or c[i][-1] < c[j][-1]):
-                        new_c = copy.deepcopy(c)
-
-                        new_c[j].append(new_c[i].pop())
+                        c[j].append(c[i].pop())
                         new_q = q + 1
 
-                        new_sig = get_sig(new_c)
+                        new_sig = get_sig(c)
 
-                        if new_sig in cache:
-                            cached_q = cache[new_sig]
-                            if new_q < cached_q:
-                                print(new_sig, new_q)
-                                cache[new_sig] = new_q
-                                new_candidates.append((new_c, new_q))
-                        else:
+                        if new_sig == target_sig:
+                            print(new_q)
+                            return
+
+                        if new_sig not in cache:
                             cache[new_sig] = new_q
+                            new_c = copy.deepcopy(c)
                             new_candidates.append((new_c, new_q))
 
+                        c[i].append(c[j].pop())
+
+        print(len(new_candidates))
         candidates = new_candidates
+
+    print("something went wrong")
 
 
 def main():
@@ -57,10 +59,9 @@ def main():
     for idx, elem in enumerate(a):
         c[elem-1].insert(0, idx+1)
 
-    target_sig = ''.join(map(str, reversed(range(1, N+1)))) + '0'*N + '0'*N + '0'*N
+    target_sig = get_sig([list(reversed(range(1, N+1))), [0]*N, [0]*N, [0]*N])
 
     solve((c, 0))
-    print(cache[target_sig])
 
 
 if __name__ == '__main__':
