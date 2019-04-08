5618572de07b950f9dfe88abfd021a9ea3a5c335
Kim Bong Min <bong-min.kim@rise-world.com>
2019-04-08 18:15:32 +0200

    nothing works

---
+++ prime_digit_sums/prime_digit_sums.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+dyn_cache = {}
+
+
+def is_prime(n):
+    if n > 45:
+        raise NotImplementedError
+
+    return n in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43]
+
+
+def init(qs):
+    res = {}
+    for q in qs:
+        q /= 10
+        res[q] = res.get(q, 0) + 1
+
+    dyn_cache[5] = res
+
+
+def calc_next_lvl(qs, n):
+    prev_res = dyn_cache[n-1]
+    res = {}
+
+    for q in qs:
+        a = q / 10
+        b = q % 10000
+        res[a] = res.get(a, 0) + prev_res.get(b, 0)
+
+    dyn_cache[n] = res
+
+
+def find_all_valid_quintuplets():
+    result = []
+
+    for i1 in xrange(9):
+        for i2 in xrange(9):
+            for i3 in xrange(9):
+                for i4 in xrange(9):
+                    for i5 in xrange(9):
+                        if (is_prime(i1+i2+i3) and
+                                is_prime(i2+i3+i4) and
+                                is_prime(i3+i4+i5) and
+                                is_prime(i1+i2+i3+i4) and
+                                is_prime(i2+i3+i4+i5) and
+                                is_prime(i1+i2+i3+i4+i5)):
+                            result.append(int(''.join(map(str, [i1, i2, i3, i4, i5]))))
+
+    return result
+
+
+def check(n):
+    digits = []
+    while n > 10:
+        digits.append(n%10)
+        n /= 10
+
+    digits.append(n)
+
+    if len(digits) >= 5:
+        for i in xrange(len(digits)-3):
+            if not is_prime(digits[i]+digits[i+1]+digits[i+2]):
+                return False
+        for i in xrange(len(digits)-4):
+            if not is_prime(digits[i]+digits[i+1]+digits[i+2]+digits[i+3]):
+                return False
+        for i in xrange(len(digits)-5):
+            if not is_prime(digits[i]+digits[i+1]+digits[i+2]+digits[i+4]+digits[i+5]):
+                return False
+
+
+
+
+def solve(q, n):
+    qs = find_all_valid_quintuplets()
+    print(qs)
+
+    init(qs)
+    calc_next_lvl(qs, 6)
+
+    res = 0
+    for k, v in dyn_cache[6].iteritems():
+        if k >= 1000:
+            res += v
+
+    print(dyn_cache)
+    print(res)
+
+
+if __name__ == '__main__':
+    q = int(raw_input())
+    n = int(raw_input())
+
+    solve(q, n)
+
