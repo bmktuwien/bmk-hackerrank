be389e536ff8fc079f21e51f065dc2cdba875179
Kim Bong Min <bong-min.kim@rise-world.com>
2019-04-16 09:58:07 +0200

    intermediate commit

--- problem_solving/problem_solving.py
+++ problem_solving/problem_solving.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from collections import *
-
+import random
 
 def go(i, g, m):
     if i in m:
@@ -37,32 +37,40 @@ def problem_solving(n, k, v):
     marked = {}
     for i in range(n):
         if i not in marked:
-            print('bla: {}'.format(i))
             marked[i] = result
 
             neighbors = [x for x in g[i] if x not in marked]
 
             neighbor = None
-            tmp = 0
-            for n in neighbors:
-                if m[n] >= tmp:
-                    tmp = m[n]
-                    neighbor = n
+
+            if neighbors:
+                x = max([m[neighbor] for neighbor in neighbors])
+                max_neighbors = [neighbor for neighbor in neighbors if m[neighbor] == x]
+                print(max_neighbors)
+
+                if len(max_neighbors) == 1:
+                    neighbor = max_neighbors[0]
+                else:
+                    neighbor = random.choice(max_neighbors)
 
             while neighbor is not None:
                 marked[neighbor] = result
 
                 neighbors = [x for x in g[neighbor] if x not in marked]
                 neighbor = None
-                tmp = 0
-                for n in neighbors:
-                    if m[n] >= tmp:
-                        tmp = m[n]
-                        neighbor = n
+
+                if neighbors:
+                    x = max([m[neighbor] for neighbor in neighbors])
+                    max_neighbors = [neighbor for neighbor in neighbors if m[neighbor] == x]
+                    print(max_neighbors)
+
+                    if len(max_neighbors) == 1:
+                        neighbor = max_neighbors[0]
+                    else:
+                        neighbor = random.choice(max_neighbors)
 
             result += 1
-            
-    print(marked)
+
     print(result)
 
 
