acb48546674c95af3631d6d431bdad8c70c573e4
Kim Bong Min <bong-min.kim@rise-world.com>
2019-04-17 19:29:46 +0200

    intermediate commit

--- problem_solving/problem_solving.py
+++ problem_solving/problem_solving.py
@@ -1,78 +1,24 @@
 # -*- coding: utf-8 -*-
 
 from collections import *
-import random
-
-def go(i, g, m):
-    if i in m:
-        return m[i]
-
-    candidates = g[i]
-
-    result = 1
-
-    for c in candidates:
-        t = go(c, g, m) + 1
-        if t > result:
-            result = t
-
-    m[i] = result
-
-    return result
 
 
 def problem_solving(n, k, v):
-    g = defaultdict(list)
+    a = []
 
     for i in range(n):
-        for j in range(i):
-            if abs(v[i]-v[j]) >= k:
-                g[j].append(i)
+        flag = False
+        for j, x in enumerate(a):
+            if abs(v[i]-v[x]) >= k:
+                flag = True
+                a[j] = i
+                break
 
-    m = {}
-    for i in range(n):
-        go(i, g, m)
+        if not flag:
+            a.append(i)
 
-    result = 0
-    marked = {}
-    for i in range(n):
-        if i not in marked:
-            marked[i] = result
-
-            neighbors = [x for x in g[i] if x not in marked]
-
-            neighbor = None
-
-            if neighbors:
-                x = max([m[neighbor] for neighbor in neighbors])
-                max_neighbors = [neighbor for neighbor in neighbors if m[neighbor] == x]
-                print(max_neighbors)
-
-                if len(max_neighbors) == 1:
-                    neighbor = max_neighbors[0]
-                else:
-                    neighbor = random.choice(max_neighbors)
-
-            while neighbor is not None:
-                marked[neighbor] = result
-
-                neighbors = [x for x in g[neighbor] if x not in marked]
-                neighbor = None
-
-                if neighbors:
-                    x = max([m[neighbor] for neighbor in neighbors])
-                    max_neighbors = [neighbor for neighbor in neighbors if m[neighbor] == x]
-                    print(max_neighbors)
-
-                    if len(max_neighbors) == 1:
-                        neighbor = max_neighbors[0]
-                    else:
-                        neighbor = random.choice(max_neighbors)
-
-            result += 1
-
-    print(result)
-
+    print(a)
+    print(len(a))
 
 if __name__ == '__main__':
     t = int(raw_input())
