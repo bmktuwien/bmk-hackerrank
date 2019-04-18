bf311a35fa23b9cab51bc3dadafdcac0bc97cdeb
Kim Bong Min <bong-min.kim@rise-world.com>
2019-04-18 17:39:22 +0200

    not working

--- problem_solving/problem_solving.py
+++ problem_solving/problem_solving.py
@@ -4,21 +4,27 @@ from collections import *
 
 
 def problem_solving(n, k, v):
-    a = []
+    g = defaultdict(lambda: ([], [])) #out,in
 
     for i in range(n):
-        flag = False
-        for j, x in enumerate(a):
-            if abs(v[i]-v[x]) >= k:
-                flag = True
-                a[j] = i
-                break
+        for j in range(i):
+            if abs(v[i]-v[j]) >= k:
+                g[j][0].append(i)
+                g[i][1].append(j)
 
-        if not flag:
-            a.append(i)
+    v_out = []
+    v_in = []
+    for i in range(n):
+        outgoing, ingoing = g[i]
 
-    print(a)
-    print(len(a))
+        if (len(outgoing) - len(ingoing)) > 0:
+            v_out.append(i)
+        elif (len(ingoing) - len(outgoing)) > 0:
+            v_in.append(i)
+
+    print(v_out)
+    print(v_in)
+
 
 if __name__ == '__main__':
     t = int(raw_input())
