ef91b44707a1d6a4333ad0aa6e285fa435442a42
Kim Bong Min <bong-min.kim@rise-world.com>
2019-04-21 13:52:47 +0200

    Not the best solution, but I passed all test cases... :P

--- bike_racers/bike_racers.py
+++ bike_racers/bike_racers.py
@@ -6,6 +6,68 @@ import os
 import sys
 import math
 
+from collections import *
+
+INF = float('inf')
+
+
+def bfs(U, V, E, pair_u, pair_v, dist):
+    Q = deque()
+
+    for u in U:
+        if pair_u[u] is None:
+            dist[u] = 0
+            Q.append(u)
+        else:
+            dist[u] = INF
+
+    dist[None] = INF
+
+    while Q:
+        u = Q.popleft()
+        if dist[u] < dist[None]:
+            for v in E[u]:
+                if dist[pair_v[v]] == INF:
+                    dist[pair_v[v]] = dist[u] + 1
+                    Q.append(pair_v[v])
+
+    return dist[None] != INF
+
+
+def dfs(u, E, pair_u, pair_v, dist):
+    if u is not None:
+        for v in E[u]:
+            if dist[pair_v[v]] == dist[u] + 1:
+                if dfs(pair_v[v], E, pair_u, pair_v, dist):
+                    pair_v[v] = u
+                    pair_u[u] = v
+                    return True
+
+        dist[u] = INF
+        return False
+    else:
+        return True
+
+
+def hopcraft_karp(U, V, E):
+    pair_u = {}
+    pair_v = {}
+    dist = defaultdict(lambda: INF)
+
+    for u in U:
+        pair_u[u] = None
+    for v in V:
+        pair_v[v] = None
+
+    matching = 0
+    while bfs(U, V, E, pair_u, pair_v, dist):
+        for u in U:
+            if pair_u[u] is None:
+                if dfs(u, E, pair_u, pair_v, dist):
+
+                    matching += 1
+    return matching
+
 
 def distance_pow2(biker, bike):
     return (biker[0]-bike[0])**2 + (biker[1]-bike[1])**2
@@ -19,26 +81,23 @@ def bikeRacers(bikers, bikes, k):
             candidates.append((distance_pow2(bikers[i], bikes[j]), i, j))
 
     candidates = sorted(candidates)
-    marked_bikers = set()
-    marked_bikes = set()
 
-    count = 0
-    max_distance = 0
+    U = set()
+    V = set()
+    E = defaultdict(set)
 
-    print(candidates)
-    for d, biker, bike in candidates:
-        if biker not in marked_bikers and bike not in marked_bikes:
-            marked_bikers.add(biker)
-            marked_bikes.add(bike)
-            count += 1
+    for i in range(len(candidates)):
+        d, biker, bike = candidates[i]
+        U.add(biker)
+        V.add(-bike)
+        E[biker].add(-bike)
 
-            if d > max_distance:
-                max_distance = d
+        if i >= k-1:
+            max_matching = hopcraft_karp(U, V, E)
 
-        if count >= k:
-            break
-
-    print(max_distance)
+            if max_matching >= k:
+                print(d)
+                return
 
 
 if __name__ == '__main__':
@@ -55,8 +114,11 @@ if __name__ == '__main__':
 
     bikes = []
 
-    for _ in xrange(n):
-        bikes.append(map(int, raw_input().rstrip().split()))
+    try:
+        for _ in xrange(n):
+            bikes.append(map(int, raw_input().rstrip().split()))
+    except Exception:
+        pass
 
     bikeRacers(bikers, bikes, k)
 
