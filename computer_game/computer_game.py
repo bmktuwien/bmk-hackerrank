6fa52b61f8c1e64b942f5d388fe951d0622c62b4
Kim Bong Min <bong-min.kim@rise-world.com>
2019-04-23 09:36:33 +0200

    nothing works damn it

--- computer_game/computer_game.py
+++ computer_game/computer_game.py
@@ -6,17 +6,25 @@ from collections import *
 from fractions import *
 
 
-def is_prime(n):
-    if n == 1:
-        return False
+def trial_division(n):
+    a = set()
+    while n % 2 == 0:
+        a.add(2)
+        n /= 2
 
-    i = 2
-    while i*i <= n:
-        if n % i == 0:
-            return False
-        i += 1
+    f = 3
 
-    return True
+    while f * f <= n:
+        if n % f == 0:
+            a.add(f)
+            n /= f
+        else:
+            f += 2
+
+    if n != 1:
+        a.add(n)
+
+    return a
 
 
 def gen_primes():
@@ -36,44 +44,13 @@ def gen_primes():
         q += 1
 
 
-def gen_edges(n, a):
-    E = defaultdict(set)
-    t = set(range(n))
-
-    count = 0
-    flag = False
-    for p in gen_primes():
-        new_t = set()
-        for i in t:
-            if a[i] % p == 0:
-                E[p].add(i)
-            else:
-                new_t.add(i)
-
-            t = new_t
-
-        if not t:
-            break
-
-        count += 1
-        if count >= 100 and not flag:
-            flag = True
-            new_t = set()
-
-            for i in t:
-                if is_prime(a[i]):
-                    E[a[i]].add(i)
-                else:
-                    new_t.add(i)
-            t = new_t
-
-
-    return E
-
-
 def computer_game(n, a, b):
-    E = gen_edges(n, a)
-    print('done')
+    count = 0
+    for i in a:
+        factors = trial_division(a[0])
+        print(factors)
+
+    print(count)
 
 
 if __name__ == '__main__':
