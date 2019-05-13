f806985a48cc823739727121983d9d8823ef6618
Kim Bong Min <bong-min.kim@rise-world.com>
2019-05-13 21:57:11 +0200

    greedy algo implemented

--- task_scheduling/solution.cpp
+++ task_scheduling/solution.cpp
@@ -1,11 +1,14 @@
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 struct Task {
-    int d;
+    int id;
     int m;
+    int d;
+    int x;
 };
 
 
@@ -17,8 +20,39 @@ int main(int argc, char** argv) {
     for (int i = 0; i < n; i++) {
         int d, m;
         cin >> d >> m;
-        tasks.push_back({d, m});
+        tasks.push_back({i, m, d, -d+m});
     }
 
+    bool all_finished = false;
+
+    int time = 1;
+    while (!all_finished) {
+        all_finished = true;
+
+        Task *max_task = nullptr;
+
+        for (auto &t : tasks) {
+            if (t.m <= 0) {
+                continue;
+            } else {
+                all_finished = false;
+
+                if (max_task == nullptr || t.x > max_task->x) {
+                    max_task = &t;
+                }
+            }
+        }
+
+        if (max_task != nullptr) {
+            max_task->m--;
+            max_task->x--;
+
+            if (max_task->m == 0) {
+                cout << "Task " << max_task->id << " finished with overtime: " << time - max_task->d << endl;
+            }
+        }
+
+        time++;
+    }
 }
 
