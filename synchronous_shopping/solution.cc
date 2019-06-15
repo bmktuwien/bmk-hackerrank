90b504ea221e1cdc25cd0e807ba932b2d304cbe4
Kim Bong Min <bong-min.kim@rise-world.com>
2019-06-15 11:27:23 +0200

    broken shit!!!

--- synchronous_shopping/solution.cc
+++ synchronous_shopping/solution.cc
@@ -4,98 +4,82 @@ using namespace std;
 
 struct Entry {
     vector<bool> sig;
-    vector<int> dist;
-    vector<bool> visited;
+    int shop;
+    int distance;
 };
 
-int min_distance(int n, vector<int>& dist, vector<bool>& visited)
-{
-    int min = INT_MAX, min_index = -1;
+vector<bool> add_signatures(vector<bool>& s1, vector<bool>& s2) {
+    vector<bool> sig(s1.size(), false);
 
-    for (int i = 0; i < n; i++) {
-        if (!visited[i] && dist[i] <= min) {
-            min = dist[i];
-            min_index = i;
-        }
-    }
-
-    return min_index;
-}
-
-vector<bool> fish_signature(int k, int i, vector<vector<int>>& fish_list) {
-    vector<bool> sig(k, false);
-
-    for (auto f : fish_list[i]) {
-        sig[f] = true;
+    for (int i = 0; i < s1.size(); i++) {
+        sig[i] = s1[i] || s2[i];
     }
 
     return sig;
 }
 
-bool is_sub_signature(const vector<bool>& s1, const vector<bool>& s2) {
-    for (int i = 0; i < s1.size(); i++) {
-        if (s1[i] == 0 && s2[i] == 1) {
-            return false;
-        }
-    }
 
-    return true;
-}
+void solve(int n, int k, vector<vector<int>>& graph, vector<vector<bool>>& signatures) {
+    auto cmp = [](Entry &left, Entry &right) { return left.distance > right.distance; };
 
-void update_dist_matrix(vector<Entry>& dist_matrix, vector<bool>& sig, int n, int k,
-                        int u, int v, int d) {
+    priority_queue<Entry, vector<Entry>, decltype(cmp)> queue(cmp);
+    set<pair<int,vector<bool>>> processed;
+    map<pair<int,vector<bool>>, int> dist_map;
 
-    for (auto &e : dist_matrix) {
-        if (is_sub_signature(e.sig, sig)) {
+    queue.push({signatures[0], 0, 0});
 
-        }
-    }
+    while (!queue.empty()) {
+        Entry e = queue.top();
+        queue.pop();
 
-    Entry e{sig, };
-    auto &dist = dist_map[sig].first;
-    auto &visited = dist_map[sig].second;
+        processed.insert(make_pair(e.shop, e.sig));
 
-    if (!visited[v] && dist[u]+d < dist[v]) {
-        dist[v] = dist[u]+d;
-    }
-}
-
-void solve(int n, vector<vector<int>>& graph, vector<vector<int>>& fish_list) {
-    map<vector<bool>, pair<vector<int>, vector<bool>>> dist;
-
-    dist[0] = 0;
-    int u = 0;
-
-    while (u != -1) {
-        visited[u] = true;
-        for (int i = 0; i < n; i++) {
-            if (graph[u][i] == 0 || visited[i]) {
+        for (int shop = 0; shop < n; shop++) {
+            if (graph[e.shop][shop] == 0) {
                 continue;
             }
 
-            if (dist[i] != INT_MAX && dist[u])
+            auto sig = add_signatures(e.sig, signatures[shop]);
+            auto key = make_pair(shop, sig);
+
+            auto it = processed.find(key);
+            if (it == processed.end()) {
+                // not processed yet
+                int d = INT_MAX;
+
+                auto it2 = dist_map.find(key);
+                if (it2 != dist_map.end()) {
+                    d = dist_map[key];
+                }
+
+                if (e.distance+graph[e.shop][shop] < d) {
+                    dist_map[key] = e.distance+graph[e.shop][shop];
+                    queue.push({sig, shop, e.distance+graph[e.shop][shop]});
+                }
+            }
         }
     }
+
 }
 
 int main(int argc, char **argv) {
     int n, m, k;
     cin >> n >> m >> k;
 
-    vector<vector<int>> fish_list;
+    vector<vector<bool>> signatures;
 
     for (int i = 0; i < n; i++) {
         int f;
         cin >> f;
 
-        vector<int> v;
+        vector<bool> sig(k, false);
         for (int j = 0; j < f; j++) {
             int ft;
             cin >> ft;
-            v.push_back(ft);
+            sig[ft-1] = true;
         }
 
-        fish_list.push_back(v);
+        signatures.push_back(sig);
     }
 
     vector<vector<int>> graph(n, vector<int>(n, 0));
@@ -106,4 +90,6 @@ int main(int argc, char **argv) {
         graph[a-1][b-1] = w;
         graph[b-1][a-1] = w;
     }
+
+    solve(n, k, graph, signatures);
 }
