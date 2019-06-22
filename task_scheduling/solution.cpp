59678d8ad6859f3f7db95dae6c396479ca8afefc
Kim Bong Min <bong-min.kim@rise-world.com>
2019-06-22 20:29:46 +0200

    broken crap

--- task_scheduling/solution.cpp
+++ task_scheduling/solution.cpp
@@ -2,45 +2,69 @@
 
 using namespace std;
 
+struct Node {
+    int key;
+    int max_overtime;
+    int lazy_propagation;
+    Node* left;
+    Node* right;
+};
+
 struct Task {
     int id;
     int m;
     int d;
-    int prio;
 };
 
-int solve(vector<Task>& tasks, int x) {
-    auto cmp = [](Task &left, Task &right) { return left.prio < right.prio; };
-
-    priority_queue<Task, vector<Task>, decltype(cmp)> queue(cmp);
-    for (int i = 0; i < x; i++) {
-        queue.push(tasks[i]);
+Node* buildBST(int start, int end) {
+    if (start > end) {
+        return nullptr;
     }
 
-    int ans = 0;
-    int time = 1;
+    int mid = (start + end)/2;
+    auto *root = new Node();
+    root->key = mid;
+    root->max_overtime = 0;
+    root->lazy_propagation = 0;
 
-    while (!queue.empty()) {
-        Task t = queue.top();
-        queue.pop();
 
-        if (time - t.d > ans) {
-            ans = time - t.d;
-        }
+    root->left = buildBST(start, mid - 1);
+    root->right = buildBST(mid + 1, end);
 
-        t.m--;
-        if (t.m > 0) {
-            t.prio--;
-            queue.push(t);
-        }
-
-        time++;
-    }
-
-    return ans;
+    return root;
 }
 
-void solve2(vector<Task>& tasks) {
+int updateBST(Node *node, Task& task, int rank, int lazy_propagation) {
+    if (node == nullptr) {
+        return 0;
+    }
+
+    int new_max;
+
+    if (rank == node->key) {
+        lazy_propagation += node->lazy_propagation;
+
+        new_max = (lazy_propagation + task.m) - task.d;
+        node->lazy_propagation += task.m;
+    } else if (rank > node->key) {
+        new_max = updateBST(node->right, task, rank, lazy_propagation + node->lazy_propagation);
+    } else {
+        node->lazy_propagation += task.m;
+
+        int m1 = node->max_overtime + lazy_propagation;
+        int m2 = updateBST(node->left, task, rank, lazy_propagation);
+
+        new_max = max(m1, m2);
+    }
+
+    if (node->max_overtime < new_max) {
+        node->max_overtime = new_max;
+    }
+
+    return node->max_overtime;
+}
+
+void solve(vector<Task>& tasks) {
     for (int i = 0; i < tasks.size(); i++) {
         vector<Task> tasks_copy(tasks.begin(), tasks.begin()+i+1);
 
@@ -64,6 +88,26 @@ void solve2(vector<Task>& tasks) {
     }
 }
 
+void solve2(vector<Task>& tasks) {
+    vector<Task> tasks_sorted(tasks.begin(), tasks.end());
+
+    std::sort(tasks_sorted.begin(), tasks_sorted.end(), [](Task &left, Task &right) {
+        return left.d < right.d;
+    });
+
+    vector<int> task_ranks(tasks_sorted.size());
+    for (int i = 0; i < tasks_sorted.size(); i++) {
+        task_ranks[tasks_sorted[i].id] = i;
+    }
+
+    auto *bst = buildBST(0, tasks_sorted.size()-1);
+
+    for (auto &task : tasks) {
+        int ans = updateBST(bst, task, task_ranks[task.id], 0);
+        cout << ans << endl;
+    }
+}
+
 int main(int argc, char** argv) {
     int n;
     cin >> n;
@@ -72,7 +116,7 @@ int main(int argc, char** argv) {
     for (int i = 0; i < n; i++) {
         int d, m;
         cin >> d >> m;
-        tasks.push_back({i, m, d, -d+m});
+        tasks.push_back({i, m, d});
     }
 
     /*for (int x = 1; x <= n; x++) {
