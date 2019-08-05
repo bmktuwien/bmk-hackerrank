a372b8fb27125dad6935debe51b6d99cc3ce8ef5
Kim Bong Min <bong-min.kim@rise-world.com>
2019-08-05 09:34:26 +0200

    Nothing works at all

--- prime_xor/solution.cc
+++ prime_xor/solution.cc
@@ -18,25 +18,55 @@ vector<bool> gen_prime_table(int n) {
 }
 
 void solve(const vector<int>& freq_tbl, const vector<bool>& prime_tbl) {
-    int N = 1000;
+    int N = 4500;
     int W = 8192;
 
-    int dp[N+1][W+1];
+    vector<vector<int>> dp(N+1, vector<int>(W+1, 0));
 
-    for (int i = 0; i <= N; i++) {
-        int a = 3500 + i - 1;
-
-        for (int j = 0; j <= W; j++) {
-            if (i == 0) {
-                dp[i][j] = 0;
+    for (int a = 0; a <= N; a++) {
+        for (int w = 0; w <= W; w++) {
+            if (a == 0) {
+                dp[a][w] = 0;
+            } else if (w == 0) {
+                dp[a][w] = 1;
             } else {
-                dp[i][j] = dp[i-1][j^a] + dp[i-1][j];
+                int freq = freq_tbl[a];
+                int c1 = freq / 2 + 1;
+                int c2 = freq / 2 + (freq % 2);
+
+                dp[a][w] = c1 * dp[a-1][w] + c2 * dp[a-1][w^a];
             }
         }
     }
+
+    int ans = 0;
+    for (int w = 2; w <= W; w++) {
+        if (prime_tbl[w] && dp[N][w] > 0) {
+            ans += dp[N][w];
+        }
+    }
+
+    cout << ans << endl;
 }
 
 int main(int argc, char **argv) {
     auto prime_table = gen_prime_table(8192);
+
+    int q;
+    cin >> q;
+
+    while (q--) {
+        int n;
+        cin >> n;
+        vector<int> freq_tbl(4501, 0);
+
+        while (n--) {
+            int i;
+            cin >> i;
+            freq_tbl[i]++;
+        }
+
+        solve(freq_tbl, prime_table);
+    }
 
 }
